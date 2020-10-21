c8bc4ee410114b6c8ad22c4beafea1247896c3c1
IvanGG-w <72536540+IvanGG-w@users.noreply.github.com>
2020-10-21 16:26:19 +0500

    Update and rename picture.cpp to sun.cpp

---
+++ sun.cpp
@@ -0,0 +1,14 @@
+
+#include "graphics.h"
+#include "picture.hpp"
+
+void sun(){
+   setfillstyle(SOLID_FILL, COLOR(255,195,85));
+   setcolor(COLOR(255,195,85));
+   fillellipse(300,300,100,100);
+   
+   setfillstyle(SOLID_FILL, COLOR(222,255,25));
+   setcolor(COLOR(222,255,25));
+   fillellipse(300,300,95,950);
+   
+}
